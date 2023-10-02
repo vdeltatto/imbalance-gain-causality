7ff4847109ee9831b825554d3710ad663a03aaa1
Vittorio Del Tatto <vdeltatt@login2.mgmt>
2023-10-02 11:38:19 +0200

    tau_test

---
+++ tau_test/analysis/ig_embeddings/compute_ig_lorenz96.py
@@ -0,0 +1,41 @@
+import numpy as np
+from dadapy.metric_comparisons import MetricComparisons
+import pickle
+import sys
+sys.path.append('/scratch/vdeltatt/imbalance-gain-causality')
+from utilities import construct_time_delay_embedding
+
+tau = int(sys.argv[1])
+epsilons = np.linspace(0.,0.3,31)
+sample_traj = pickle.load(open(f"../../../fig3_errors/trajs/lorenz96/seed0_ieps0.p","rb"))
+N = 5000
+D = 40
+n_jobs = 8
+k = 20
+alphas = np.linspace(0.,1.5,50)
+
+# parameters for time-delay embeddings
+E = 30
+tau_e = 1
+sample_times = np.linspace(100000,350000,N,dtype=int)
+
+info_imbalances_X_to_Y = np.zeros((len(epsilons), len(alphas)))
+info_imbalances_Y_to_X = np.zeros((len(epsilons), len(alphas)))
+
+for ieps, eps in enumerate(epsilons):
+    trajectory = pickle.load(open(f"../../../fig3_errors/trajs/lorenz96/seed0_ieps{ieps}.p","rb"))
+    assert trajectory.shape == sample_traj.shape
+
+    X_time_delay, Y_time_delay = construct_time_delay_embedding(X=trajectory[:,1], Y=trajectory[:,D+1], E=E, tau_e=tau_e)
+    X0 = X_time_delay[sample_times]
+    Y0 = Y_time_delay[sample_times]
+    Xtau = X_time_delay[sample_times+tau]
+    Ytau = Y_time_delay[sample_times+tau]
+
+    d = MetricComparisons(maxk=len(sample_times)-1, njobs=n_jobs)
+    info_imbalances_X_to_Y[ieps] = d.return_inf_imb_causality(
+        cause_present=X0, effect_present=Y0, effect_future=Ytau, weights=alphas, k=k)
+    info_imbalances_Y_to_X[ieps] = d.return_inf_imb_causality(
+        cause_present=Y0, effect_present=X0, effect_future=Xtau, weights=alphas, k=k)
+
+pickle.dump([info_imbalances_X_to_Y, info_imbalances_Y_to_X], open(f"./pickles/lorenz96/tau{tau}.p","wb"))
---
+++ tau_test/analysis/ig_embeddings/compute_ig_lorenz96.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=tau_emb #name of job
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=12:00:00 #max time required
+#SBATCH --partition=regular1,regular2
+#SBATCH --output=./log_sbatch/%x.o%j              # Standard output
+#SBATCH --error=./log_sbatch/%x.o%j               # Standard error
+#SBATCH --mail-type=FAIL                 # Mail events (NONE, BEGIN, END, FAIL, ALL). Sends you an email when the job begins, ends, or fails; you can combine options.
+#SBATCH --mail-user=vdeltatt@sissa.it    # Where to send the mail
+#send using parallel sbatch compute_ig_lorenz96.sh ::: 1 2 3 4 5 10 15 20 25 30 50 70 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500
+
+python3 compute_ig_lorenz96.py ${1}
