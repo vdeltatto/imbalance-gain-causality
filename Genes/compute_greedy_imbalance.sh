435a7623a57f8942556e115a86dc162ed76b55f4
Vittorio Del Tatto <vdeltatt@login2.mgmt>
2023-10-30 18:38:03 +0100

    Add genes subdir

---
+++ Genes/compute_greedy_imbalance.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=greedy #name of job
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=12:00:00 #max time required
+#SBATCH --partition=regular1,regular2
+#SBATCH --output=./log_sbatch/%x.o%j              # Standard output
+#SBATCH --error=./log_sbatch/%x.o%j               # Standard error
+#SBATCH --mail-type=NONE                 # Mail events (NONE, BEGIN, END, FAIL, ALL). Sends you an email when the job begins, ends, or fails; you can combine options.
+#SBATCH --mail-user=vdeltatt@sissa.it    # Where to send the mail
+#send using parallel sbatch compute_greedy_imbalance.sh ::: {1..30} ::: {1 5 10 20}
+
+python3 compute_greedy_imbalance.py --n_coords 30 --k ${1} --n_best ${2}
